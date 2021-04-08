88181c650a727555940d52c601de7cb1485159b6
JiyaSachdeva <74546515+JiyaSachdeva@users.noreply.github.com>
2021-04-08 12:15:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg ;
+
+function preload() {
+backgroundImg = loadImage(bg);
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+  if (backgroundImg)
+    background (backgroundImg);
+    Engine.update(engine);
+    getBackgroundImg();
+  
+
+
+}
+
+async function getBackgroundImg(){
+var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+var responsej =  await response.json();
+var datetime = responsej.datetime;
+var hour = datetime.slice(11,13);
+console.log(hour);
+
+if (hour>=04 && hour<=6){
+    bg = "sunrise1.png";
+}else if (hour>=06 && hour<=08){
+    bg = "sunrise2.png";
+} else if (hour>=8 && hour<=10){
+    bg = "sunrise3.png";
+}else if (hour>=10 && hour<=12){
+    bg = "sunrise4.png";
+}else if (hour>=12 && hour<=14){
+    bg = "sunrise5.png";
+}else if (hour>=14 && hour<=16){
+    bg = "sunrise6.png";
+} else if (hour>=16 && hour<=18){
+    bg = "sunset7.png";
+} else if (hour>=18 && hour<=20){
+    bg = "sunset8.png";
+} else if (hour>=20 && hour<=22){
+    bg = "sunset9.png";
+} else if (hour>=22 && hour<=0){
+    bg = "sunset10.png";
+} else if (hour>=0 && hour<=2){
+    bg = "sunset11.png";
+} else if (hour>=2 && hour<=4){
+    bg = "sunset12.png";
+}
+
+
+
+}
